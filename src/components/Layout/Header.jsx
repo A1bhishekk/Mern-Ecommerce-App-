0aa26ad98e4c77c7a3e68eab5343c396f3e776d2
Abhishek <sahniak56@gmail.com>
2023-03-25 03:36:00 +0530

    mern stack ecommerce

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+const Header = () => {
+    return (
+        <>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Technical Abhi</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <NavLink to="/" className="nav-link active" aria-current="page" >Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/register" className="nav-link" >Register</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/login" className="nav-link" >Login</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink to="/cart" className="nav-link" >Cart (0)</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Header
